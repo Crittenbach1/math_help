c493a3811fcb36dd4633ebe14fdea707b2867a9a
Cynthia Rittenbach <crittenbach@gmail.com>
2018-05-25 01:10:07 -0400

    added application controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < Sinatra::Base
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+
+
+end
