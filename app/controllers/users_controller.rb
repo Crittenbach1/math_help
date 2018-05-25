f57a6e44f55c0fe458b694eefdb33f9315aefd2e
Cynthia Rittenbach <crittenbach@gmail.com>
2018-05-25 19:08:57 -0400

    post /users/new creates new user, sets session id, redirects to user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,13 @@ class ApplicationController < Sinatra::Base
 
      get '/users/new' do
        erb :'users/signup'
-     end 
+     end
+
+     post '/users/new' do
+       @user = User.create(email: params[:email], password: params[:password])
+       session[:user_id] = @user.id
+       redirect '/users/profile'
+     end
 
 
 end
