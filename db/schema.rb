417cfc7c6a01f5f6e163a90294bb6c67488cb4ee
Cynthia Rittenbach <crittenbach@gmail.com>
2018-05-25 20:28:44 -0400

    new create_topics migration/table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180525215446) do
+ActiveRecord::Schema.define(version: 20180526002631) do
+
+  create_table "topics", force: :cascade do |t|
+    t.integer "user_id"
+    t.string  "title"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
