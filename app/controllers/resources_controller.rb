203ef4ea45a9aab7096070227d2753e3c54b1adb
Cynthia Rittenbach <crittenbach@gmail.com>
2018-05-25 21:50:39 -0400

    post /Resources/delete deletes resource if it's topic is owned by current user

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -15,4 +15,15 @@ class ApplicationController < Sinatra::Base
     redirect "/users/profile"
   end
 
+  post '/Resources/delete/:id' do
+    @resource = Resource.find(params[:id].to_i)
+    @topic = Topic.find(@resource.topic_id)
+    if @topic.user_id == Helper.current_user(session).id
+      @resource.delete
+    else
+      erb :error
+    end
+    redirect 'users/profile'
+  end
+
 end
