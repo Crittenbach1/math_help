3e08f04638c2104e8bfe89d5042329a7efe5185c
Cynthia Rittenbach <crittenbach@gmail.com>
2018-05-25 20:04:12 -0400

    post /login authenticates user and goes to profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,17 @@ class ApplicationController < Sinatra::Base
   get '/login' do
     erb :'users/login'
   end
-  
+
+  post '/login' do
+    user = User.find_by(email: params["email"])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/users/profile'
+    else
+      redirect '/login'
+    end 
+  end
+
 
 end
