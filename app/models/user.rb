7443366e3d414c08d50c45c21997fd523cfc9bf3
Cynthia Rittenbach <crittenbach@gmail.com>
2018-05-25 20:22:36 -0400

    added user form validations for presence and uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :topics
+  validates :email, :password_digest, :presence => true
+  validates :email, :password_digest, :uniqueness => true
 
 
 end
